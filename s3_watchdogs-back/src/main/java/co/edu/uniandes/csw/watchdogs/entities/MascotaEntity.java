fd30a12ac60ab4de7278918bf0e2915f426e1eac
ca.beltran10 <ca.beltran10@ISIS2603S3-0078.sis.virtual.uniandes.edu.co>
2018-02-12 08:08:53 -0500

    Se creo e implemento MascotaEntity

---
+++ s3_watchdogs-back/src/main/java/co/edu/uniandes/csw/watchdogs/entities/MascotaEntity.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.watchdogs.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author ca.beltran10
+ */
+@Entity
+public class MascotaEntity implements Serializable{
+
+    //----- Atributos -----
+    
+    private String color;
+    private String raza;
+    private String sexo;
+    private String nombre;
+    
+    @Id
+    private Long id;
+
+    
+    //----- Metodos -----
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * @return the raza
+     */
+    public String getRaza() {
+        return raza;
+    }
+
+    /**
+     * @param raza the raza to set
+     */
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
