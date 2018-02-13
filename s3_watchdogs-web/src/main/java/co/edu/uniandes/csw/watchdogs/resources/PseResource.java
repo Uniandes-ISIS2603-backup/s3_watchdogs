f7e255ba67434f9191586503dc99f0cc64131e4e
jc.pulido <jc.pulido@ISIS2603S3-0065.sis.virtual.uniandes.edu.co>
2018-02-13 11:44:10 -0500

    Se corrigieron la clases Pse resource y tarjetaCredito Resource

--- s3_watchdogs-web/src/main/java/co/edu/uniandes/csw/watchdogs/resources/PseResource.java
+++ s3_watchdogs-web/src/main/java/co/edu/uniandes/csw/watchdogs/resources/PseResource.java
@@ -7,23 +7,31 @@ package co.edu.uniandes.csw.watchdogs.resources;
 
 import co.edu.uniandes.csw.watchdogs.dtos.PseDetailDTO;
 import co.edu.uniandes.csw.watchdogs.exceptions.BusinessLogicException;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 /**
  *
  * @author jc.pulido
  */
+
+@Path("pse")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
 public class PseResource {
     
     /**
-     * <h1>POST /api/cities : Crear una ciudad.</h1>
+     * <h1>POST /api/pse : Crear una ciudad.</h1>
      * 
-     * <pre>Cuerpo de petición: JSON {@link CityDetailDTO}.
+     * <pre>Cuerpo de petición: JSON.
      * 
      * Crea una nueva ciudad con la informacion que se recibe en el cuerpo 
      * de la petición y se regresa un objeto identico con un id auto-generado 
@@ -31,15 +39,15 @@ public class PseResource {
      * 
      * Codigos de respuesta:
      * <code style="color: mediumseagreen; background-color: #eaffe0;">
-     * 200 OK Creó la nueva ciudad .
+     * 200 OK Creó el nuevo método de pago PSE.
      * </code>
      * <code style="color: #c7254e; background-color: #f9f2f4;">
-     * 412 Precodition Failed: Ya existe la ciudad.
+     * 412 Precodition Failed: Ya existe el método de pago.
      * </code>
      * </pre>
-     * @param pse {@link CityDetailDTO} - La ciudad que se desea guardar.
-     * @return JSON {@link CityDetailDTO}  - La ciudad guardada con el atributo id autogenerado.
-     * @throws BusinessLogicException {@link BusinessLogicExceptionMapper} - Error de lógica que se genera cuando ya existe la ciudad.
+     * @param pse {@link PseDetailDTO} -El método de pago Pse que se desea guardar.
+     * @return JSON {@link PseDetailDTO}  - El método de pago Pse con el atributo id autogenerado.
+     * @throws BusinessLogicException {@link BusinessLogicException} - Error de lógica que se genera cuando ya existe el método de pago pse.
      */
     @POST
     public PseDetailDTO createPse(PseDetailDTO pse) throws BusinessLogicException
@@ -48,20 +56,20 @@ public class PseResource {
     }
     
     /**
-     * <h1>GET /api/cities/{id} : Obtener ciudad por id.</h1>
+     * <h1>GET /api/pse/{id} : Obtener ciudad por id.</h1>
      * 
-     * <pre>Busca la ciudad con el id asociado recibido en la URL y la devuelve.
+     * <pre>Busca el método de pago pse con el id asociado recibido en la URL y la devuelve.
      * 
      * Codigos de respuesta:
      * <code style="color: mediumseagreen; background-color: #eaffe0;">
-     * 200 OK Devuelve la ciudad correspondiente al id.
+     * 200 OK Devuelve el pago pse correspondiente al id.
      * </code> 
      * <code style="color: #c7254e; background-color: #f9f2f4;">
-     * 404 Not Found No existe una ciudad con el id dado.
+     * 404 Not Found No existe un pago pse con el id dado.
      * </code> 
      * </pre>
-     * @param id Identificador de la ciudad que se esta buscando. Este debe ser una cadena de dígitos.
-     * @return JSON {@link CityDetailDTO} - La ciudad buscada
+     * @param id Identificador del pago pse que se esta buscando. Este debe ser una cadena de dígitos.
+     * @return JSON {@link PseDetailDTO} - El pago buscado
      */
     @GET
     @Path("(id: \\d+)")
@@ -71,21 +79,21 @@ public class PseResource {
     }
     
       /**
-     * <h1>PUT /api/cities/{id} : Actualizar ciudad con el id dado.</h1>
-     * <pre>Cuerpo de petición: JSON {@link CityDetailDTO}.
+     * <h1>PUT /api/pse/{id} : Actualizar pago pse con el id dado.</h1>
+     * <pre>Cuerpo de petición: JSON {@link PseDetailDTO}.
      * 
-     * Actualiza la ciudad con el id recibido en la URL con la informacion que se recibe en el cuerpo de la petición.
+     * Actualiza el pago pse con el id recibido en la URL con la informacion que se recibe en el cuerpo de la petición.
      * 
      * Codigos de respuesta:
      * <code style="color: mediumseagreen; background-color: #eaffe0;">
-     * 200 OK Actualiza la ciudad con el id dado con la información enviada como parámetro. Retorna un objeto identico.</code> 
+     * 200 OK Actualiza el pago pse con el id dado con la información enviada como parámetro. Retorna un objeto identico.</code> 
      * <code style="color: #c7254e; background-color: #f9f2f4;">
-     * 404 Not Found. No existe una ciudad con el id dado.
+     * 404 Not Found. No existe un pago pse con el id dado.
      * </code> 
      * </pre>
-     * @param id Identificador de la ciudad que se desea actualizar.Este debe ser una cadena de dígitos.
-     * @param pse {@link CityDetailDTO} La ciudad que se desea guardar.
-     * @return JSON {@link CityDetailDTO} - La ciudad guardada.
+     * @param id Identificador del pago pse que se desea actualizar.Este debe ser una cadena de dígitos.
+     * @param pse {@link PseDetailDTO} La ciudad que se desea guardar.
+     * @return JSON {@link PseDetailDTO} - La ciudad guardada.
      * @throws BusinessLogicException {@link BusinessLogicExceptionMapper} - Error de lógica que se genera al no poder actualizar la ciudad porque ya existe una con ese nombre.
      */
     @PUT
@@ -95,22 +103,22 @@ public class PseResource {
     }
     
      /**
-     * <h1>DELETE /api/cities/{id} : Borrar ciudad por id.</h1>
+     * <h1>DELETE /api/pse/{id} : Borrar pago pse por id.</h1>
      * 
-     * <pre>Borra la ciudad con el id asociado recibido en la URL.
+     * <pre>Borra el pago pse con el id asociado recibido en la URL.
      * 
      * Códigos de respuesta:<br>
      * <code style="color: mediumseagreen; background-color: #eaffe0;">
-     * 200 OK Elimina la ciudad correspondiente al id dado.</code>
+     * 200 OK Elimina el pago pse correspondiente al id dado.</code>
      * <code style="color: #c7254e; background-color: #f9f2f4;">
-     * 404 Not Found. No existe una ciudad con el id dado.
+     * 404 Not Found. No existe un pago pse con el id dado.
      * </code>
      * </pre>
-     * @param id Identificador de la ciudad que se desea borrar. Este debe ser una cadena de dígitos.
+     * @param id Identificador del pago pse que se desea borrar. Este debe ser una cadena de dígitos.
      */
     @DELETE
     @Path("{id: \\d+}")
-     public void deleteCity(@PathParam("id") Long id) {
+     public void deletePse(@PathParam("id") Long id) {
         // Void
     }
 }
