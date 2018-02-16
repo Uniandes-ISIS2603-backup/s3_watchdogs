0263d6d0db1d6c5aa38e4ef17426a2fc78510f53
jc.pulido <jc.pulido@ISIS2603S3-0065.sis.virtual.uniandes.edu.co>
2018-02-16 10:53:14 -0500

    Algunas clases entity creadas

---
+++ s3_watchdogs-back/src/main/java/co/edu/uniandes/csw/watchdogs/entities/PayPalEntity.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.watchdogs.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author jc.pulido
+ */
+@Entity
+public class PayPalEntity extends BaseEntity implements Serializable{
+    
+    private String correo;
+    private String contrasenha;
+
+    /**
+     * Método que retorna el correo
+     * @return El correo asociado 
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * Método que asinga un nuevo valor al correo
+     * @param correo Correo nuevo a asignar 
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    /**
+     * Método que retorna la contrasenha 
+     * @return La contrasenha
+     */
+    public String getContrasenha() {
+        return contrasenha;
+    }
+
+    /**
+     * Método que asigna un nuevo valor a la contrasenha 
+     * @param contrasenha Contraseña nueva a asignar
+     */
+    public void setContrasenha(String contrasenha) {
+        this.contrasenha = contrasenha;
+    }
+    
+    
+    
+}
---
+++ s3_watchdogs-back/src/main/java/co/edu/uniandes/csw/watchdogs/entities/PseEntity.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.watchdogs.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author jc.pulido
+ */
+@Entity
+public class PseEntity extends BaseEntity implements Serializable {
+    
+    private String correo;
+    
+    /**
+     * Método que retorna el correo asociado
+     * @return El correo 
+     */
+    public String getCorreo()
+    {
+        return correo;
+    }
+    
+    /**
+     * Método que asigna un nuevo valor al correo
+     * @param correo Correo a asignar 
+     */
+    public void setCorreo(String correo)
+    {
+        this.correo = correo;
+    }
+    
+}
