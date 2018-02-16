7c9cc091de274da1a4c51386546eeb8ff6e82ebb
ca.beltran10 <ca.beltran10@ISIS2603S3-0078.sis.virtual.uniandes.edu.co>
2018-02-16 10:19:54 -0500

    Se crea la clase ClienteEntity implementada con extencion de BaseEntity

---
+++ s3_watchdogs-back/src/main/java/co/edu/uniandes/csw/watchdogs/entities/ClienteEntity.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.watchdogs.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author ca.beltran10
+ */
+@Entity
+public class ClienteEntity extends BaseEntity implements Serializable {
+
+    private String cedula;
+
+    /**
+     * @return the cedula
+     */
+    public String getCedula() {
+        return cedula;
+    }
+
+    /**
+     * @param cedula the cedula to set
+     */
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    
+}
