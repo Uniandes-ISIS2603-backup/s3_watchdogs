873eba50d1bd27f728ea444711b8e9ea8b13f60d
c.martinezc1 <c.martinezc1@uniandes.edu.co>
2018-03-06 23:19:33 -0500

    idsfix

---
+++ s3_watchdogs-web/sql/deletes.sql
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  c.martinezc1
+ * Created: 6/03/2018
+ */
+delete from ServicioEntity_RutaEntity;
+delete from RutaEntity;
+delete from LugarEntity;
+delete from FacturaEntity;
+delete from ServicioEntity;
+delete from TransporteEntity;
+delete from MascotaEntity;
+delete from EmpleadoEntity;
+delete from DisponibilidadEntity;
+delete from ClienteEntity_MetodoDePagoEntity;
+delete from MetodoDePagoEntity;
+delete from ClienteEntity;
+delete from CalificacionEntity;
